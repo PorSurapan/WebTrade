c0a707e947a9309ee30b6834089a69e486d8f0d6
ninesam12 <naynine789@gmail.com>
2021-02-21 20:48:42 +0700

    update trade

--- trade.php
+++ trade.php
@@ -3,17 +3,20 @@
 
     $own = $_POST['pOwn'];
     $proID = $_POST['proID'];
+    $pID = $_POST['pID'];
     
 	$con = new mysqli("localhost", "root", "","trader");
 	
-	$sql = "INSERT INTO trade (sender,receiver)
-			VALUES('"  . $own . "', '" . $proID . "')";
+	$sql = "INSERT INTO trade (sender,receiver,product)
+			VALUES('"  . $own . "', '" . $proID . "', '" . $pID . "')";
 
         echo $sql;
     
     if($con->query($sql))
     {
-         header("Location: home.php");
+        echo '<script type="text/javascript">';
+        echo 'if(!alert("อัพเดตข้อมูลแล้ว!")) document.location = "home.php";';
+        echo '</script>';
     }
     else
     {
