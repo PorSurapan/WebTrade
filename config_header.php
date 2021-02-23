498078dc2c4f23f5c061998ed7d08d7e34d1d959
PorSurapan <por0839360604@gmail.com>
2021-02-23 15:00:01 +0700

    null

--- config_header.php
+++ config_header.php
@@ -1,19 +1,25 @@
-
-<?php 
-    
+<?php
     $conn=mysqli_connect("localhost", "root", "","trader");
     $conn->query("SET NAMES UTF8");
-    $sql = "SELECT * FROM profiles WHERE username = " . " '" . $_SESSION['s_username'] . "' ";
-    $rs = $conn->query($sql);
 
+    $check = isset($_SESSION['s_username']);
+
+    
+    if ($check == true)
+    {
+        $session = $_SESSION['s_username'];
+
+        $sql = "SELECT * FROM profiles WHERE username = '" . $session . "'";
+        $rs = $conn->query($sql);
+    
         while($row = $rs->fetch_assoc()) {
             $role = $row['roles'];
 
             if ($role == "ผู้ลงเทรด")
-            include("header_sender.html");
+                include("header_sender.html");
             else 
-            include("header_receiver.html");
+                include("header_receiver.html");
         }
-
-        $conn->close();
+    }
+    $conn->close();
 ?>
