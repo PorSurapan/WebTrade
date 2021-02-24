e963266ee1fb74df6d478f6a1837147fa97b8511
Theerawat61 <forworkonly42@gmail.com>
2021-02-24 12:19:34 +0700

    c

---
+++ sender_chart.php
@@ -0,0 +1,25 @@
+<link rel="stylesheet" href = "chart.css">
+<table id="q-graph">
+<caption>Quarterly Results</caption>
+<thead>
+<tr>
+<th></th>
+<th class="sent">Invoiced</th>
+<th class="paid">Collected</th>
+</tr>
+</thead>
+<tbody>
+<tr class="qtr" id="q1">
+<th scope="row">Q1</th>
+<td class="sent bar" style="height: 111px;"><p>$18,450.00</p></td>
+<td class="paid bar" style="height: 99px;"><p>$16,500.00</p></td>
+</tr>
+</table>
+
+<div id="ticks">
+<div class="tick" style="height: 59px;"><p>$50,000</p></div>
+<div class="tick" style="height: 59px;"><p>$40,000</p></div>
+<div class="tick" style="height: 59px;"><p>$30,000</p></div>
+<div class="tick" style="height: 59px;"><p>$20,000</p></div>
+<div class="tick" style="height: 59px;"><p>$10,000</p></div>
+</div>
